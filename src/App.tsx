8f988a609c298d8bda0cbf57cf3b5f4a019ac39a
ricardocardoso90 <84364125+ricardocardoso90@users.noreply.github.com>
2022-11-23 20:58:37 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { Movie } from './Types';
 
 function App() {
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const getMovies = async () => {
+    const response = await axios.get('https://api.b7web.com.br/cinema/');
+    const json = response.data;
+    setMovies(json);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='box'>
+      <header className='header'>
+        <h3>Total de Filmes em Cartaz: {movies.length}</h3>
       </header>
+      <section className='container'>
+
+        <div className='grid'>
+          {movies.map((item, index) => {
+            return (
+              <div key={index} className='grid-movies'>
+                <img src={item.avatar} alt="" width={170} />
+                {/* {item.titulo} */}
+              </div>
+            )
+          })}
+        </div>
+
+      </section>
     </div>
-  );
-}
+  )
+};
 
 export default App;
